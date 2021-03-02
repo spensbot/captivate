47a72ef292daf6ebc9239f96ad4ce18f50c7ba76
Spenser Saling <spenser0saling@gmail.com>
2021-03-01 22:47:17 -0800

    Fix Param Slider bug by adding height style

--- src/components/controls/ParamSlider.tsx
+++ src/components/controls/ParamSlider.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import {useDispatch} from 'react-redux'
-import { setBaseParams } from '../../redux/scenesSlice'
 import {ParamKey} from '../../engine/params'
-import { useRealtimeSelector } from '../../redux/realtimeStore'
-import { useTypedSelector } from '../../redux/store'
 import MySlider from '../base/MySlider'
 
 interface Props {
@@ -12,12 +9,8 @@ interface Props {
 
 export default function ParamSlider({paramKey}: Props) {
 
-  // const param = useRealtimeSelector(state => state.outputParams[paramKey])
-  const param = 0.5
-  const dispatch = useDispatch()
-
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', alignSelf: 'stretch', marginRight: '1rem' }}>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '10rem', marginRight: '1rem' }}>
       <div style={{ flex: '1 0 10rem' }}>
         <MySlider paramKey={paramKey} orientation="vertical"
         />
