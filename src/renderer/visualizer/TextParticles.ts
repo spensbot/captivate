c7e748a172623c994c4d427d8ba41fe36da12515
spensbot <spenser0saling@gmail.com>
2022-01-24 19:23:22 -0800

    IDK more stuff

--- src/renderer/visualizer/TextParticles.ts
+++ src/renderer/visualizer/TextParticles.ts
@@ -5,6 +5,12 @@ import shaders from './shaders'
 import { particles } from './particles'
 import { textOutlineShapesAndHoles } from './text'
 
+const attrib = {
+  position: 'position',
+  color: 'customColor',
+  size: 'size',
+}
+
 interface Config {
   text: string
   textSize: number
@@ -18,6 +24,7 @@ export default class TextParticles extends VisualizerBase {
   readonly type = 'TextParticles'
   particle = particles.circle
   particles = new THREE.Points()
+  startPoints: THREE.Vector3[] = []
   geometryCopy = new THREE.BufferGeometry()
   raycaster = new THREE.Raycaster()
   colorChange = new THREE.Color()
@@ -44,6 +51,16 @@ export default class TextParticles extends VisualizerBase {
   }
 
   update({ time }: UpdateResource): void {
+    // console.log(this.particles.geometry.attributes)
+    // console.log(this.particles.geometry.attributes.position)
+    // console.log(this.particles.geometry.attributes.customColor)
+    // console.log(this.particles.geometry.attributes.size)
+    // const pos = this.particles.geometry.attributes.position
+    // const startPos =
+    // if (pos === undefined) return
+    // for (let i = 0; i < pos.count; i++) {
+    //   pos.setXYZ(i, , , pos.getZ(i) + 0.01)
+    // }
     // this.planeArea.rotation.x = time.beats
     // const pos = this.particles.geometry.attributes.position
     // const copy = this.geometryCopy.attributes.poisiton
@@ -76,11 +93,11 @@ export default class TextParticles extends VisualizerBase {
     // geoParticles.translate(xMid, yMid, 0)
 
     geoParticles.setAttribute(
-      'customColor',
+      attrib.color,
       new THREE.Float32BufferAttribute(colors, 3)
     )
     geoParticles.setAttribute(
-      'size',
+      attrib.size,
       new THREE.Float32BufferAttribute(sizes, 1)
     )
 
