7808e135fe4e60e766098e8db0985b0cfe4e9b5f
spensbot <spenser0saling@gmail.com>
2022-05-24 09:18:27 -0700

    Add new snapToMultipleOf2() and getMultiplier() for use in visualizer

--- src/visualizer/threejs/util/util.ts
+++ src/visualizer/threejs/util/util.ts
@@ -1,5 +1,7 @@
 import { PerspectiveCamera } from 'three'
 import convert from 'color-convert'
+import { Normalized } from 'types/baseTypes'
+import { lerp } from '../../../shared/util'
 
 export function colorFromHSV(h: number, s: number, v: number) {
   return Number('0x' + convert.hsv.hex([h * 360, s * 50, v * 100]))
@@ -18,4 +20,33 @@ export function visibleSizeAtZ(depth: number, camera: PerspectiveCamera) {
     width: height * camera.aspect,
     height: height,
   }
+}
+
+export function snapToMultipleOf2(val: number) {
+  const LOOP_DEPTH = 10
+  let i = 0
+  let ref = 1
+  let dif = Math.abs(val - ref)
+  let relativeDif = dif / ref
+  while (relativeDif > 0.55 && i < LOOP_DEPTH) {
+    if (val > ref) {
+      ref *= 2
+    } else {
+      ref /= 2
+    }
+    i += 1
+  }
+  return ref
+}
+
+export function getMultiplier(epicness: Normalized, obey: Normalized) {
+  // parabola from 0 to 1
+  let skewed = epicness ** 3
+
+  // parabola from 0 to 7
+  // < 0.5 returns 0.125 - 1.0
+  // > 0.5 returns 1.0 - 7.125
+  let mapped = skewed * 7
+
+  return lerp(1, mapped, obey)
 }
