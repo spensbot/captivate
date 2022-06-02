60671e45d40c430f923ed6d72ab657702713dcc2
spensbot <spenser0saling@gmail.com>
2022-06-01 21:34:42 -0700

    Remove everything except for the src folder to prepare for migration to the new git repo

--- electron/fileServer.ts
+++
@@ -1,6 +0,0 @@
-const http = require('http')
-const express = require('express')
-const expressApp = express()
-const cors = require('cors')
-const router = express.Router()
-
--- tests/test.ts
+++
@@ -1,8 +0,0 @@
-import testValidation from './testValidation'
-import testValidation2 from './testValidation2'
-
-// Sad day, I'm giving up on this for now :/
-// testValidation()
-
-// But now, I'm trying again!
-// testValidation2()
