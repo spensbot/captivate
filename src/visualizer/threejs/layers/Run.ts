8e4e713605b50b9c11f98f521925facae5dd3b03
spensbot <spenser0saling@gmail.com>
2022-06-06 21:14:10 -0700

    Fix ts stuff. Add x64 build to mac

--- src/visualizer/threejs/layers/Run.ts
+++ src/visualizer/threejs/layers/Run.ts
@@ -3,12 +3,12 @@ import {Vector3} from 'three'
 import LayerBase from './LayerBase'
 import UpdateResource from '../UpdateResource'
 import { zeroArray } from '../../../shared/util'
-import { Range } from '../../../math/range'
+// import { Range } from '../../../math/range'
 
-const Y_RANGE: Range = {
-  min: -7,
-  max: 3
-} // units
+// const Y_RANGE: Range = {
+//   min: -7,
+//   max: 3
+// } // units
 const STEP = 0.5 // units
 const RADIANS = Math.PI/2
 const RADIUS = 100 // units
@@ -27,19 +27,19 @@ export function initRunConfig(): RunConfig {
 }
 
 export default class Run extends LayerBase {
-  private config: RunConfig
+  // private config: RunConfig
   private material = new THREE.LineDashedMaterial({ color: 0x00ff00, linewidth: 5})
   private line: THREE.Line
 
-  constructor(config: RunConfig) {
+  constructor(_config: RunConfig) {
     super()
     this.line = section(this.material)
     this.scene.add(this.line)
-    this.config = config
+    // this.config = config
   }
 
-  update({ time }: UpdateResource): void {
-    
+  update({}: UpdateResource): void {
+    firstRow()
   }
 
   dispose() {
