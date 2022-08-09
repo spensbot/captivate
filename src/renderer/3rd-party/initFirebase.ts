65c46023820ae7606957cbc7697f740ee3cee69e
spensbot <spenser0saling@gmail.com>
2022-08-09 00:16:55 -0700

    Another attempt to fix ci builds

--- src/renderer/3rd-party/initFirebase.ts
+++ src/renderer/3rd-party/initFirebase.ts
@@ -1,7 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app'
 import * as firebase from 'firebase/compat/app'
-import * as firebaseApp from 'firebase/app'
 import {
   getAnalytics,
   logEvent as logEventWithAnalytics,
@@ -25,7 +23,7 @@ const firebaseConfig = {
 }
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig)
+const app = firebase.default.initializeApp(firebaseConfig)
 const analytics = getAnalytics(app)
 
 export function logEvent(
