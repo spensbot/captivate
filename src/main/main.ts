0f6b3e93c4326c8d9a704305c5cdbb9be6205a4f
spensbot <spenser0saling@gmail.com>
2022-08-21 19:29:15 -0700

    Add REDUX_DEVTOOLS extension

--- src/main/main.ts
+++ src/main/main.ts
@@ -8,68 +8,67 @@
  * When running `npm run build` or `npm run build:main`, this file is compiled to
  * `./src/main.js` using webpack. This gives us some performance wins.
  */
-import path from 'path';
-import { app, BrowserWindow, shell, dialog } from 'electron';
-import { autoUpdater } from 'electron-updater';
-import log from 'electron-log';
-import MenuBuilder from './menu';
-import { resolveHtmlPath } from './util';
-import * as engine from './engine/engine';
-import { VisualizerContainer } from './engine/createVisualizerWindow';
-import './prevent_sleep';
-import './electron_error_logging';
-
+import path from 'path'
+import { app, BrowserWindow, shell, dialog } from 'electron'
+import { autoUpdater } from 'electron-updater'
+import log from 'electron-log'
+import MenuBuilder from './menu'
+import { resolveHtmlPath } from './util'
+import * as engine from './engine/engine'
+import { VisualizerContainer } from './engine/createVisualizerWindow'
+import './prevent_sleep'
+import './electron_error_logging'
 export default class AppUpdater {
   constructor() {
-    log.transports.file.level = 'info';
-    autoUpdater.logger = log;
-    autoUpdater.checkForUpdatesAndNotify();
+    log.transports.file.level = 'info'
+    autoUpdater.logger = log
+    autoUpdater.checkForUpdatesAndNotify()
   }
 }
 
-let mainWindow: BrowserWindow | null = null;
-let isClosing = false;
+let mainWindow: BrowserWindow | null = null
+let isClosing = false
 let visualizerContainer: VisualizerContainer = {
   visualizer: null,
-};
+}
 
 if (process.env.NODE_ENV === 'production') {
-  const sourceMapSupport = require('source-map-support');
-  sourceMapSupport.install();
+  const sourceMapSupport = require('source-map-support')
+  sourceMapSupport.install()
 }
 
 const isDevelopment =
-  process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
+  process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true'
 
 if (isDevelopment) {
-  require('electron-debug')({ showDevTools: false });
+  require('electron-debug')({ showDevTools: false })
 }
 
 const installExtensions = async () => {
-  const installer = require('electron-devtools-installer');
-  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-  const extensions = ['REACT_DEVELOPER_TOOLS'];
+  const installer = require('electron-devtools-installer')
+  const forceDownload = !!process.env.UPGRADE_EXTENSIONS
+  const extensions = ['REACT_DEVELOPER_TOOLS', 'REDUX_DEVTOOLS']
 
   return installer
     .default(
       extensions.map((name) => installer[name]),
       forceDownload
     )
-    .catch(console.log);
-};
+    .catch(console.log)
+}
 
 const createWindow = async () => {
   if (isDevelopment) {
-    await installExtensions();
+    await installExtensions()
   }
 
   const RESOURCES_PATH = app.isPackaged
     ? path.join(process.resourcesPath, 'assets')
-    : path.join(__dirname, '../../assets');
+    : path.join(__dirname, '../../assets')
 
   const getAssetPath = (...paths: string[]): string => {
-    return path.join(RESOURCES_PATH, ...paths);
-  };
+    return path.join(RESOURCES_PATH, ...paths)
+  }
 
   mainWindow = new BrowserWindow({
     show: false,
@@ -83,25 +82,25 @@ const createWindow = async () => {
       webSecurity: false,
       nodeIntegration: false,
     },
-  });
+  })
 
-  mainWindow.loadURL(resolveHtmlPath('index.html'));
+  mainWindow.loadURL(resolveHtmlPath('index.html'))
 
   mainWindow.on('ready-to-show', () => {
     if (!mainWindow) {
-      throw new Error('"mainWindow" is not defined');
+      throw new Error('"mainWindow" is not defined')
     }
     if (process.env.START_MINIMIZED) {
-      mainWindow.minimize();
+      mainWindow.minimize()
     } else {
-      mainWindow.show();
+      mainWindow.show()
     }
-  });
+  })
 
   mainWindow.on('close', (e) => {
     if (!isClosing) {
-      e.preventDefault();
-      if (mainWindow === null) return;
+      e.preventDefault()
+      if (mainWindow === null) return
 
       dialog
         .showMessageBox(mainWindow, {
@@ -112,37 +111,34 @@ const createWindow = async () => {
         })
         .then(({ response }) => {
           if (response === 1) {
-            isClosing = true;
-            mainWindow?.close();
-            engine.stop();
-            mainWindow = null;
-            app.quit();
+            isClosing = true
+            mainWindow?.close()
+            engine.stop()
+            mainWindow = null
+            app.quit()
           }
         })
         .catch((err) => {
-          console.error(`showMessageBox err: `, err);
-        });
+          console.error(`showMessageBox err: `, err)
+        })
     }
-  });
+  })
 
   // Open urls in the user's browser
   mainWindow.webContents.on('new-window', (event, url) => {
-    event.preventDefault();
-    shell.openExternal(url);
-  });
+    event.preventDefault()
+    shell.openExternal(url)
+  })
 
-  const ipcCallbacks = engine.start(
-    mainWindow.webContents,
-    visualizerContainer
-  );
+  const ipcCallbacks = engine.start(mainWindow.webContents, visualizerContainer)
 
-  const menuBuilder = new MenuBuilder(mainWindow, { ipcCallbacks });
-  menuBuilder.buildMenu();
+  const menuBuilder = new MenuBuilder(mainWindow, { ipcCallbacks })
+  menuBuilder.buildMenu()
 
   // Remove this if your app does not use auto updates
   // eslint-disable-next-line
-  new AppUpdater();
-};
+  new AppUpdater()
+}
 
 /**
  * Add event listeners...
@@ -159,11 +155,11 @@ const createWindow = async () => {
 app
   .whenReady()
   .then(() => {
-    createWindow();
+    createWindow()
     app.on('activate', () => {
       // On macOS it's common to re-create a window in the app when the
       // dock icon is clicked and there are no other windows `open`.
-      if (mainWindow === null) createWindow();
-    });
+      if (mainWindow === null) createWindow()
+    })
   })
-  .catch(console.log);
+  .catch(console.log)
